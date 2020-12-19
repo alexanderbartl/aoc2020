d094ceaef2f7ec158cf4e05da4cbb7c682b0b2f4
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-19 13:32:59 +0100

    Day 19, part 2 fixed

--- 19/B.py
+++ 19/B.py
@@ -1,7 +1,5 @@
 import re
 
-# between 267 and 369?; not 329, 314
-
 with open('input.txt') as f:
     rules, messages = f.read().split('\n\n')
 rules = {line.split(': ')[0]: line.split(': ')[1].replace('"', '') for line in rules.split('\n')}
@@ -19,16 +17,13 @@ def evaluate_rule(rule):
 
 
 def match_rule0(message):
-    # rule 0 = 8 11 = 42 42+ 31+
-    for i in range(1, 15):
-        for j in range(2, 25):
-            if re.match(f"^{rule42 * j}{rule31 * i}$".replace(' ', ''), message):
-                return True
+    # rule 0 = 8 11 = 42+ 42{n}31{n} with n>0 unknown
+    for i in range(1, 20):
+        if re.match(f"^({rule42})+{rule42 * i}{rule31 * i}$".replace(' ', ''), message):
+            return True
     return False
 
 
 rule42 = f"({evaluate_rule('42')})"
 rule31 = f"({evaluate_rule('31')})"
-print(rule42)
-print(rule31)
 print(len([message for message in messages if match_rule0(message)]))
