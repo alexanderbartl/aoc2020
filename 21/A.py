e0dec3c37e60fdc78f66427362050987b326b13c
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-21 07:35:31 +0100

    Day 21, simplify code

--- 21/A.py
+++ 21/A.py
@@ -8,23 +8,15 @@ class Food:
 with open('input.txt') as f:
     foods = [Food(line) for line in f.readlines()]
 
-all_allergens = set.union(*(f.allergens for f in foods))
-all_ingredients = set.union(*(f.ingredients for f in foods))
-not_containing = {}
 possibly_containing = {}
-for allergen in all_allergens:
-    not_containing[allergen] = set.union(*(f.ingredients for f in foods if allergen in f.allergens)) \
-                               - set.intersection(*(f.ingredients for f in foods if allergen in f.allergens))
+for allergen in set.union(*(f.allergens for f in foods)):
     possibly_containing[allergen] = set.intersection(*(f.ingredients for f in foods if allergen in f.allergens))
 
-not_containing_anything = set()
-for allergen, ingredients in not_containing.items():
-    somewhere_else = set.union(*possibly_containing.values())
-    not_containing_anything = not_containing_anything.union(
-        set([ingredient for ingredient in ingredients if ingredient not in somewhere_else]))
+# part 1
+print(len([ingredient for f in foods for ingredient in f.ingredients
+           if ingredient not in set.union(*(i for i in possibly_containing.values()))]))
 
-print(sum(1 if ingredient in f.ingredients else 0 for f in foods for ingredient in not_containing_anything))
-
+# part 2
 while any(len(ingredients) > 1 for ingredients in possibly_containing.values()):
     for allergen in possibly_containing.keys():
         possibly_containing[allergen] = possibly_containing[allergen] - set(
