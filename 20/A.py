8c3bf1f19d1e6682bf458c5490fe96e47ed86ed0
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-22 18:11:44 +0100

    Day 20: Rewrite part A using a Tile class

--- 20/A.py
+++ 20/A.py
@@ -4,35 +4,51 @@ with open('input.txt') as f:
     raw_tiles = f.read().split('\n\n')
 
 
-def fingerprint(string):
-    return int(string.replace('.', '0').replace('#', '1'), 2)
+class Tile:
+    def __init__(self, raw):
+        lines = raw.strip().split('\n')
+        self.id = int(lines[0][5:9])
+        self.img = lines[1:]
+        self.fingerprints = [self.fingerprint_left(), self.fingerprint_top(), self.fingerprint_right(), self.fingerprint_bottom()]
+
+    def __repr__(self):
+        return f'<Tile id={self.id}>'
+
+    def get_next(self, direction):
+        fingerprint = getattr(self, f'fingerprint_{direction}')()
+        candidates = [t for t in tiles if t.id != self.id and fingerprint in t.fingerprints
+                      or self._reversed(fingerprint) in t.fingerprints]
+        if len(candidates) != 1:
+            return None
+        return candidates[0]
+
+    def fingerprint_left(self):
+        return self._fingerprint(''.join(l[0] for l in self.img))
+
+    def fingerprint_top(self):
+        return self._fingerprint(self.img[0])
+
+    def fingerprint_right(self):
+        return self._fingerprint(''.join(l[-1] for l in self.img))
+
+    def fingerprint_bottom(self):
+        return self._fingerprint(self.img[-1])
+
+    def rotate(self):
+        self.img = [''.join(l) for l in list(zip(*self.img[::-1]))]
+
+    def flip(self):
+        self.img = self.img[::-1]
+
+    @staticmethod
+    def _fingerprint(string):
+        return int(string.replace('.', '0').replace('#', '1'), 2)
+
+    @staticmethod
+    def _reversed(n):
+        return int(bin(n)[2:].zfill(10)[::-1], 2)
 
 
-def parse_tile(raw_tile):
-    lines = raw_tile.strip().split('\n')
-    id = int(lines[0][5:9])
-    border_top = fingerprint(lines[1].strip())
-    border_bottom = fingerprint(lines[10].strip())
-    border_left = fingerprint(''.join(l[0] for l in lines[1:]))
-    border_right = fingerprint(''.join(l[9] for l in lines[1:]))
-    border_top_r = fingerprint(lines[1].strip()[::-1])
-    border_bottom_r = fingerprint(lines[10].strip()[::-1])
-    border_left_r = fingerprint(''.join(l[0] for l in lines[1:])[::-1])
-    border_right_r = fingerprint(''.join(l[9] for l in lines[1:])[::-1])
-    return id, (border_top, border_right, border_bottom, border_left, border_top_r, border_right_r, border_bottom_r, border_left_r)
-
-
-def flip_fingerprint(n):
-    return int(bin(n)[:1:-1], 2)
-
-
-tiles = dict(parse_tile(t) for t in raw_tiles)
-all_borders = [b for borders in tiles.values() for b in borders]
-corners = []
-for id, borders in tiles.items():
-    matches = sum(all_borders.count(b) for b in borders[:4])
-    if matches == 6:
-        corners.append(id)
-
-print(corners)
-print(math.prod(corners))
+tiles = [Tile(t) for t in raw_tiles]
+corners = [t for t in tiles if len([neighbor for neighbor in (t.get_next('right'), t.get_next('top'), t.get_next('left'), t.get_next('bottom')) if neighbor is not None]) == 2]
+print(corners, math.prod(t.id for t in corners))
