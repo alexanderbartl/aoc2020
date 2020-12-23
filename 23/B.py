b34ec7c12cc1517277a246a04beef60535f17291
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-23 12:13:24 +0100

    Day 23, part 2 - runtime down to 1.5h

--- 23/B.py
+++ 23/B.py
@@ -2,28 +2,30 @@ from collections import deque
 from tqdm import tqdm
 
 INPUT = "496138527"
-
-
-def get_destination_index(number):
-    try:
-        return cups.index(number) + 1
-    except ValueError:
-        return None
+CACHE_EVERY = 1000
 
 
 cups = deque([int(x) for x in INPUT] + list(range(10, 1_000_001)))
-for _ in tqdm(range(10_000_000)):
+last_seen = {}
+for i in tqdm(range(10_000_000)):
+    if i % CACHE_EVERY == 0:
+        last_seen = {number: pos for pos, number in enumerate(cups)}
     current_cup = cups.popleft()
     cups.append(current_cup)
+    last_seen[current_cup] = len(cups)
     three_cups = (cups.popleft(), cups.popleft(), cups.popleft())
-    try:
-        destination = get_destination_index(current_cup - 1) or \
-                  get_destination_index(max(cup for cup in cups if cup < cups[-1])) or \
-                  get_destination_index(max(cups))
-    except ValueError:
-        destination = get_destination_index(max(cups))
+    for j in range(1, 5):
+        destination_cup = current_cup - j if current_cup > j else current_cup - j + max(cups)
+        if destination_cup not in three_cups:
+            try:
+                destination = cups.index(destination_cup, last_seen[destination_cup] - 100, last_seen[destination_cup]) + 1
+            except ValueError:
+                destination = cups.index(destination_cup, max(0, last_seen[destination_cup] - 4 * (i % CACHE_EVERY) - 10), last_seen[destination_cup]) + 1
+            break
+
     for cup in three_cups[::-1]:
         cups.insert(destination, cup)
+        last_seen[cup] = destination
 
 cups.rotate(-cups.index(1))
 print(cups[1] * cups[2])
