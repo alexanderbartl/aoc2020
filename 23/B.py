2aaca953a18781172df67db89f0c7ff8c3cc4e1f
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-23 06:29:34 +0100

    Day 23, part 2 - too slow

---
+++ 23/B.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+INPUT = "496138527"
+
+
+def get_destination_index(number):
+    try:
+        return cups.index(number) + 1
+    except ValueError:
+        return None
+
+
+cups = deque([int(x) for x in INPUT] + list(range(10, 1_000_001)))
+for _ in range(10_000_000):
+    current_cup = cups.popleft()
+    cups.append(current_cup)
+    three_cups = (cups.popleft(), cups.popleft(), cups.popleft())
+    destination = get_destination_index(current_cup - 1) or \
+                  get_destination_index(max(cup for cup in cups if cup < cups[-1])) or \
+                  get_destination_index(max(cups))
+    for cup in three_cups[::-1]:
+        cups.insert(destination, cup)
+
+    print(_)
+
+cups.rotate(-cups.index(1))
+print(cups[1] * cups[2])
