f13e5a57f1bb480cbaaeb33d253a460f31be88d5
Alexander Bartl <alexander.bartl@tngtech.com>
2020-12-24 08:57:31 +0100

    Day 23, part 2 rewrite
    Based on a data structure inspired by Jan's solution, the code now runs 30s rather than 1.5h

--- 23/B.py
+++ 23/B.py
@@ -1,31 +1,34 @@
-from collections import deque
 from tqdm import tqdm
 
 INPUT = "496138527"
-CACHE_EVERY = 1000
 
 
-cups = deque([int(x) for x in INPUT] + list(range(10, 1_000_001)))
-last_seen = {}
+class Cup:
+    next = None
+
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f'{self.value} → {self.next.value}'
+
+
+initial_order = [int(x) for x in INPUT] + list(range(10, 1_000_001))
+cups = {value: Cup(value) for value in initial_order}
+for value, next_value in zip(initial_order, initial_order[1:] + initial_order[:1]):
+    cups[value].next = cups[next_value]
+
+
+current_cup = cups[initial_order[0]]
 for i in tqdm(range(10_000_000)):
-    if i % CACHE_EVERY == 0:
-        last_seen = {number: pos for pos, number in enumerate(cups)}
-    current_cup = cups.popleft()
-    cups.append(current_cup)
-    last_seen[current_cup] = len(cups)
-    three_cups = (cups.popleft(), cups.popleft(), cups.popleft())
-    for j in range(1, 5):
-        destination_cup = current_cup - j if current_cup > j else current_cup - j + max(cups)
-        if destination_cup not in three_cups:
-            try:
-                destination = cups.index(destination_cup, last_seen[destination_cup] - 100, last_seen[destination_cup]) + 1
-            except ValueError:
-                destination = cups.index(destination_cup, max(0, last_seen[destination_cup] - 4 * (i % CACHE_EVERY) - 10), last_seen[destination_cup]) + 1
-            break
+    to_move = [current_cup.next, current_cup.next.next, current_cup.next.next.next]
+    next_cup = to_move[-1].next
+    destination_value = current_cup.value - 1 if current_cup.value > 1 else max(initial_order)
+    while destination_value in [cup.value for cup in to_move]:
+        destination_value = destination_value - 1 if destination_value > 1 else max(initial_order)
+    to_move[-1].next = cups[destination_value].next
+    cups[destination_value].next = to_move[0]
+    current_cup.next = next_cup
+    current_cup = current_cup.next
 
-    for cup in three_cups[::-1]:
-        cups.insert(destination, cup)
-        last_seen[cup] = destination
-
-cups.rotate(-cups.index(1))
-print(cups[1] * cups[2])
+print(cups[1].next.value * cups[1].next.next.value)
